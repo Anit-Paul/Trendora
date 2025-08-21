f9aa6bac58023bba0821b9df5e6217036af9de55
Anit Paul <anit42655@gmail.com>
2025-08-22 00:30:01 +0530

    password hide feature added for loginPage

--- frontend/src/components/login/login.jsx
+++ frontend/src/components/login/login.jsx
@@ -2,8 +2,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { AiOutlineLogin } from "react-icons/ai";
 import { Link } from "react-router-dom";
 import styles from "./login.module.css";
+import { GoEye } from "react-icons/go";
+import { GoEyeClosed } from "react-icons/go";
+import { useState } from "react";
 
 function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+  function managePasswordState() {
+    setShowPassword(!showPassword);
+  }
   return (
     <div className={styles.loginContainer}>
       <div className={styles.loginCard}>
@@ -25,11 +32,18 @@ function Login() {
               Password
             </label>
             <input
-              type="password"
+              type={showPassword ? "text" : "password"}
               className={styles.formInput}
               id="password"
               placeholder="Enter your password"
             />
+            <span className={styles.icon}>
+              {showPassword ? (
+                <GoEye onClick={managePasswordState} />
+              ) : (
+                <GoEyeClosed onClick={managePasswordState} />
+              )}
+            </span>
           </div>
           <button type="submit" className={styles.loginButton}>
             <AiOutlineLogin />
