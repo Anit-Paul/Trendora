2762d4ca9a4ce1ef2b162e1121c2e5a50d0dea12
Anit Paul <anit42655@gmail.com>
2025-08-23 19:02:43 +0530

    Hero page changing content animation added

--- frontend/src/components/hero/Hero.jsx
+++ frontend/src/components/hero/Hero.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import css from "./hero.module.css";
 import { FaCircle } from "react-icons/fa";
+
 function Hero() {
   const content = [
     "New Season, New Styles - Explore the Latest Collection",
@@ -8,7 +9,15 @@ function Hero() {
     "Exclusive Collection – Limited Stock Available!",
     "Upgrade Your Wardrobe with Everyday Essentials",
   ];
+  
   const [page, changePage] = useState(1);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      changePage((cur) => (cur % 4) + 1);
+    }, 2000);
+
+    return () => clearInterval(interval); // cleanup
+  }, []);
 
   return (
     <div className={css.heroContainer}>
@@ -16,24 +25,28 @@ function Hero() {
         <h1>{content[page - 1]}</h1>
         <div className="myIcons">
           <FaCircle
+            color={page == 1 ? "#3498db" : "#2c3e50"}
             key={1}
             onClick={() => {
               changePage(1);
             }}
           />
           <FaCircle
+            color={page == 2 ? "#3498db" : "#2c3e50"}
             key={2}
             onClick={() => {
               changePage(2);
             }}
           />
           <FaCircle
+            color={page == 3 ? "#3498db" : "#2c3e50"}
             key={3}
             onClick={() => {
               changePage(3);
             }}
           />
           <FaCircle
+            color={page == 4 ? "#3498db" : "#2c3e50"}
             key={4}
             onClick={() => {
               changePage(4);
