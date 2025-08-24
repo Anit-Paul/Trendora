d93d20a8cea1ad450a19c93fc3403413d3264e5e
Anit Paul <anit42655@gmail.com>
2025-08-24 12:09:44 +0530

    nav added

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,20 +2,23 @@ import { Routes, Route } from "react-router-dom";
 import { useContext } from "react";
 import Login from "./components/login/login";
 import adminContext from "./store/admin";
-
+import Nav from "./components/nav/nav";
 function App() {
   const { admin } = useContext(adminContext);
 
   return !admin ? (
     <Login />
   ) : (
-    <Routes>
-      <Route path="/home" element={<h1>Home</h1>} />
-      <Route path="/add" element={<h1>Add</h1>} />
-      <Route path="/list" element={<h1>List</h1>} />
-      <Route path="/order" element={<h1>Order</h1>} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
+    <>
+      <Nav />
+      <Routes>
+        <Route path="/home" element={<h1>Home</h1>} />
+        <Route path="/add" element={<h1>Add</h1>} />
+        <Route path="/list" element={<h1>List</h1>} />
+        <Route path="/order" element={<h1>Order</h1>} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
   );
 }
 
